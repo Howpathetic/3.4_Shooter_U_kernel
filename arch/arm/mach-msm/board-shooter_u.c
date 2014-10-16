28279a34d423578d0b14931fe01d443041405a01
Howpathetic <jakub.skopal1@gmail.com>
2014-10-16 09:44:04 +0200

    Added camera sensors and sp3d

--- arch/arm/mach-msm/board-shooter_u.c
+++ arch/arm/mach-msm/board-shooter_u.c
@@ -1,4 +1,5 @@
 /* Copyright (c) 2010-2011, Code Aurora Forum. All rights reserved.
+ * Copyright (c) 2013 Sebastian Sob <jakub.skopal1@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
